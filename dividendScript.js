ddc5fd53f98e0d01832b2b2a5d6fc62f1330aa6b
Alex Pappa <alex.pappa.1@gmail.com>
2022-03-25 13:27:32 -0400

    no dependencies

--- dividendScript.js
+++ dividendScript.js
@@ -1,41 +1,91 @@
-function retireMentCalculator(desiredRetirementAge, birthday,principal) {
-    const moment = require('moment');
-    let standardDividend = .05;
-    let monthlyReturn = 0.00;
-    let monthlyContribution = 500.00;
-    let stockPrice = 7.66// this is the average price of GUT since its inception every quarter.
-    let shares = principal/stockPrice
-    let monthsInvested = 0;
-    birthday = moment(birthday);
-    let today = moment();
-    let currentAgeInMonths = (today.diff(birthday, "months"));
-    let currentAge = (today.diff(birthday, "months") / 12).toString().replace(/\.\d+/, "");
-    let monthsToRetire = (desiredRetirementAge - currentAgeInMonths / 12) * 12;
-    let stats = [];
-    let totalInvested = 0;
-
-    while (monthsInvested <= monthsToRetire) {
-        let newObj = {};
-        switch (currentAge >= 50) {
-            case true:
-                standardDividend = .06;
-                monthlyContribution = 7000/12;
-            default:
-                totalInvested += monthlyContribution;
-                principal += monthlyContribution+monthlyReturn;
-                shares += (monthlyContribution+monthlyReturn)/stockPrice;
-                
-                monthlyReturn = standardDividend*shares;
-                monthsInvested++;
-                
-                newObj.age = currentAge
-                newObj.currentPrincipal = principal.toFixed(2);
-                stats.push(newObj);
-                today = today.add(1, "months")
-                currentAge = (today.diff(birthday, "months") / 12).toString().replace(/\.\d+/, "");
-                break;
-        }
-    }
-    return {"Total Shares":shares.toFixed(2),"Monthly Dividend":  monthlyReturn.toFixed(2), "Total Principal": principal.toFixed(),"Total Invested":totalInvested.toFixed(2)/*,"Stats":stats*/}
+function monthDifference(dateFrom, dateTo) {
+    return dateTo.getMonth() - dateFrom.getMonth() + (12 * (dateTo.getFullYear() - dateFrom.getFullYear()));
 }
-retireMentCalculator(65,"07/26/1993",14000);
+
+function retirementDateCalc(desiredAge, birthday) {
+    let d = new Date(birthday);
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let day = d.getDate();
+    return new Date(year + desiredAge, month, day);
+}
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+function getLongMonthName(monthIndex) {
+    return monthNames[monthIndex];
+}
+
+function main(desiredRetirementAge, birthday, currentShares, currentCostBasis, currentAccountBalance) {
+    //calculating date objects to figure out how many months left to retire, how old we currently are in years and in months
+    let today = new Date();
+    birthday = new Date(birthday)
+    let currentAgeInMonths = monthDifference(birthday, today);
+    let currentAge = (Math.floor(currentAgeInMonths / 12));
+    let retirementDate = retirementDateCalc(desiredRetirementAge, birthday);
+    let monthsLeftUntillRetirement = monthDifference(today, retirementDate);
+    let currentDate = new Date(today);
+    let monthsInvested = 0
+
+    //end of date calculations
+
+    //stock information
+    let sharePrice = 7.66 //average stock price of GUT over the life of the stock
+    let dividendPayout = .05;
+    let monthlyContribution = 500.00;
+    //end of stock information
+    let stats = {"Age":{}};
+
+    while (monthsInvested < monthsLeftUntillRetirement) {
+
+        if (currentAge >= 30) {
+            dividendPayout = .06
+        }
+
+        if (currentAge >= 50) {
+            dividendPayout = .07
+            monthlyContribution = 7000 / 12;
+        }
+
+        //monthly dividend calculation
+        let thisMonthsDividend = (currentShares * dividendPayout);
+        let dripShares = (thisMonthsDividend / sharePrice);
+
+        let monthlyBoughtShares = (monthlyContribution / sharePrice);
+
+        currentShares += monthlyBoughtShares + dripShares;
+        //account balance calculation
+        currentCostBasis += monthlyContribution
+        currentAccountBalance += thisMonthsDividend + monthlyContribution
+
+        //month and age calculation
+        currentDate = new Date(currentDate.setMonth(currentDate.getMonth() + 1))
+        currentAgeInMonths = monthDifference(birthday, currentDate);
+        if (currentAgeInMonths % 12 === 0) currentAge += 1
+        monthsInvested += 1;
+
+        if ((currentAge in stats["Age"]) === false) {
+            stats["Age"][currentAge] = {}
+        }
+        if ((currentDate.getFullYear() in stats["Age"][currentAge]) === false) {
+            stats["Age"][currentAge][currentDate.getFullYear()] = {}
+        }
+        stats["Age"][currentAge][currentDate.getFullYear()][getLongMonthName(currentDate.getMonth())] = {
+            "Current Cost Basis" : currentCostBasis.toFixed(2),
+            "Current Account Balance": currentAccountBalance.toFixed(2),
+            "Current Shares": currentShares.toFixed(3),
+            "Monthly Dividend": (currentShares * dividendPayout).toFixed(2)
+            
+        };
+    }
+    stats["Final"] =  {
+        "Current Cost Basis" : currentCostBasis.toFixed(2),
+        "Current Account Balance": currentAccountBalance.toFixed(2),
+        "Current Shares": currentShares.toFixed(3),
+        "Monthly Dividend": (currentShares * dividendPayout).toFixed(2)
+        
+    };
+    return stats["Final"]
+}
+console.log(main(60, "07/26/1993", 2882.212, 19674.88, 21357.19))
